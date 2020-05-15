0258b5fca8dc491e643eb515d5b8572ea2e218f9
Peter Laxalt <pete@laxaltandmciver.co>
2020-05-15 15:42:24 -0400

    update team bios

--- dist/assets/scripts/scripts.js
+++ dist/assets/scripts/scripts.js
@@ -1,76 +1,8 @@
-'use strict';
-
-// Team slider functions
-
-var slider = $('#team-slider .inner .container');
-
-var sliderContainer = $('#team-slider');
-
-$(document).ready(function () {
-  slider.slick({
-    arrows: false,
-    adaptiveHeight: true
-  });
-});
-
-$('div[data-nav="next"]').click(function () {
-  slider.slick('slickNext');
-});
-
-$('div[data-nav="prev"]').click(function () {
-  slider.slick('slickPrev');
-});
-
-$('div[data-nav="exit"]').click(function () {
-  toggleSlider();
-});
-
-$('div[data-member]').click(function () {
-  goToSlide($(this).attr('data-member'));
-  toggleSlider();
-});
-
-function goToSlide(slideIndex) {
-  slider.slick('slickGoTo', slideIndex + ', ' + true);
-}
-
-function toggleSlider() {
-  if (sliderContainer.hasClass('visible-slider')) {
-    sliderContainer.removeClass('visible-slider');
-  } else {
-    sliderContainer.addClass('visible-slider');
-  }
-}
-
-// Nav scroll functions
-
-$(window).scroll(function () {
-  var scrollPosition = $('body, html').scrollTop();
-  if (scrollPosition != 0) {
-    $('header svg').removeClass('large');
-    $('header nav').removeClass('no-scroll');
-    $('header').removeClass('no-background');
-  } else {
-    $('header svg').addClass('large');
-    $('header nav').addClass('no-scroll');
-    $('header').addClass('no-background');
-  }
-});
-
-// Contact modal functions
-$('div[data-contact="exit"]').click(function () {
-  toggleModal();
-});
-
-$('div[data-contact="open"]').click(function () {
-  toggleModal();
-});
-
-function toggleModal() {
-  var modal = $('#contact-modal');
-  if (modal.hasClass('visible-modal')) {
-    modal.removeClass('visible-modal');
-  } else {
-    modal.addClass('visible-modal');
-  }
-}
+/**
+* jekyll-starter-kit v0.1.1
+* https://github.com/amazingSurge/jekyll-starter-kit
+*
+* Copyright (c) amazingSurge <amazingSurge@gmail.com>
+* Released under the MIT license
+*/
+"use strict";var slider=$("#team-slider .inner .container"),sliderContainer=$("#team-slider");function goToSlide(e){slider.slick("slickGoTo",e+", "+!0)}function toggleSlider(){sliderContainer.hasClass("visible-slider")?sliderContainer.removeClass("visible-slider"):sliderContainer.addClass("visible-slider")}function toggleModal(){var e=$("#contact-modal");e.hasClass("visible-modal")?e.removeClass("visible-modal"):e.addClass("visible-modal")}$(document).ready(function(){slider.slick({arrows:!1,adaptiveHeight:!0})}),$('div[data-nav="next"]').click(function(){slider.slick("slickNext")}),$('div[data-nav="prev"]').click(function(){slider.slick("slickPrev")}),$('div[data-nav="exit"]').click(function(){toggleSlider()}),$("div[data-member]").click(function(){goToSlide($(this).attr("data-member")),toggleSlider()}),$(window).scroll(function(){0!=$("body, html").scrollTop()?($("header svg").removeClass("large"),$("header nav").removeClass("no-scroll"),$("header").removeClass("no-background")):($("header svg").addClass("large"),$("header nav").addClass("no-scroll"),$("header").addClass("no-background"))}),$('div[data-contact="exit"]').click(function(){toggleModal()}),$('div[data-contact="open"]').click(function(){toggleModal()});
